e693ae886fd7d01ad35d5e46da16a884f2ee2d3c
ZetaZ3ta <71658714+ZetaZ3ta@users.noreply.github.com>
2021-01-07 22:06:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/catcopernicfinalvictorpaz/FragmentC.kt
@@ -0,0 +1,23 @@
+package com.example.catcopernicfinalvictorpaz
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_a.view.*
+import kotlinx.android.synthetic.main.fragment_c.view.*
+
+class FragmentC : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_c, container, false)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/catcopernicfinalvictorpaz/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.catcopernicfinalvictorpaz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.About -> {
+                Toast.makeText(this, "Victor Paz",Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
